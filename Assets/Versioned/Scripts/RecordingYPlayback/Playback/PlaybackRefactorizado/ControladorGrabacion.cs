f2cc912f26bc2cfb501dee06c00972f06dc74ae4
Franco Arellano <francoare@live.com>
2022-06-12 15:52:52 -0300

    refactorizado 2

---
+++ Assets/Versioned/Scripts/RecordingYPlayback/Playback/PlaybackRefactorizado/ControladorGrabacion.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ControladorGrabacion : MonoBehaviour
+{
+    
+    [SerializeField] private EstructuraGrabacion _estructuraGrabacion;
+    public InicializadorGrabaciones inicializadorGrabaciones;
+    public Reproductor _reproductor;
+    
+    //paths de los archivos
+    public string pathHeader;
+    public string pathGrabacion;
+    public string pathClima;
+
+    //public float velocidadReproduccion = 1;
+    
+    private int recordingFps;
+    private int cantidadIntervalos;
+    
+    private int climaYToDFPS;
+    private int cantidadIntervalosClimaYToD;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            AbirGrabacion();
+            ReproducirGrabacion(0.3f, true);
+        } 
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            ToggleInterpolar();
+            Debug.Log("Interpolacion Cambiada");
+        } 
+            
+    }
+
+    public void ComenzarPlayback(float velocidadReproduccion, bool interpolar)
+    {
+        AbirGrabacion();
+        ReproducirGrabacion(velocidadReproduccion, interpolar);
+    }
+    
+    private void AbirGrabacion()
+    {
+        inicializadorGrabaciones.InicializarGrabacion(pathHeader, _estructuraGrabacion, _reproductor);
+        inicializadorGrabaciones.CargarFrames(pathGrabacion, pathClima, _estructuraGrabacion);
+        
+        recordingFps = inicializadorGrabaciones.recordingFps;
+        cantidadIntervalos = inicializadorGrabaciones.cantidadIntervalos;
+        climaYToDFPS = inicializadorGrabaciones.climaYToDFPS;
+        cantidadIntervalosClimaYToD = inicializadorGrabaciones.cantidadIntervalosClimaYToD;
+    }
+
+    private void ReproducirGrabacion(float velocidadReproduccion, bool interpolar)
+    {
+        SetearVelocidad(velocidadReproduccion);
+        _reproductor.interpolar = interpolar;
+        _reproductor.Reproducir();
+    }
+
+    public void SetearVelocidad(float velocidadReproduccion)
+    {
+        float deltaIntervalosRecording = 1/(recordingFps * velocidadReproduccion);
+        float deltaIntervalosClima = 1/(climaYToDFPS * velocidadReproduccion);
+        _reproductor.deltaIntervalosRecording = deltaIntervalosRecording;
+        _reproductor.deltaIntervalosClima = deltaIntervalosClima;
+    }
+
+    public void ToggleInterpolar()
+    {
+        _reproductor.interpolar = !_reproductor.interpolar;
+    }
+    
+    
+    
+}
