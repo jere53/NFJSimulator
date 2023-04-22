5fa7a6698f68dc9c81a6ef37cb136c4a69b18da3
Juan Agustin Dure Faccini <juanchidure@outlook.com>
2023-04-22 14:51:20 -0300

    Add files

---
+++ Assets/PlaybackControls.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class PlaybackControls : MonoBehaviour
+{
+    // Basic controls
+    public Button playButton;
+    public Button pauseButton;
+    public Button stopButton;
+    public Button interpolarButton;
+    public Button fastForwardButton;
+    
+    // Camera controls
+    public Button camera1Button;
+    public Button camera2Button;
+    public Button camera3Button;
+}
